1b8d49badcc1661fcc60bed719b4ff6cb5497277
Manuel <manuel.speranza@thunder-x.thx>
2025-10-03 20:24:01 +0200

    lab1 part5 ex1

--- Lab 1/lab1-p5.py
+++ Lab 1/lab1-p5.py
@@ -15,6 +15,7 @@ def starNetwork(N):
         switch = net.addSwitch( f's{i}' )
         net.addLink( host, switch )
         net.addLink( router, switch )
+        net.cmd('pingall')
         host.cmd('ip route add default via 10.10.10.1')
 
     router.cmd('echo 1 > /proc/sys/net/ipv4/ip_forward')
