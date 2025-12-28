74d155761eeb58c13a801917081553d9e00dab05
Manuel <manuel.speranza@thunder-x.thx>
2025-12-28 15:41:40 +0100

    init

--- init.sh
+++ init.sh
@@ -1,4 +1,2 @@
-sudo mn -c
-sudo ~/lab4_script.sh
-sudo chmod 777 /home/advnet/Desktop/lab4/logs
-sudo python3 ~/Desktop/lab4/lab4_network.py $1
+sudo ~/lab6_script.sh
+sudo chmod 777 /home/advnet/Desktop/lab6/logs
