749e1a3e190f1643a1d3e6f13aafbadc1c08b255
Manuel <manuel.speranza@thunder-x.thx>
2025-12-28 16:56:02 +0100

    init

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ~/lab6_script.sh
+sudo mkdir /home/advnet/Desktop/lab6/logs
 sudo chmod 777 /home/advnet/Desktop/lab6/logs
 sudo mkdir /home/advnet/Desktop/lab6/run
 sudo python3 ~/Desktop/lab6/Lab6_Network.py
